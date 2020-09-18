5dd0b2ccb8532631fcadfc26d49ead3f1061a2e9
Miquel De Cáceres <miquelcaceres@gmail.com>
2020-09-18 12:47:43 +0200

    getLandscapeVariable

--- R/DefinePlotMethods.R
+++ R/DefinePlotMethods.R
@@ -1,76 +1,7 @@
-.getAllowedVars <-function() {
-  return(c("lct", "basalArea", "SWE", "WTD","texture1", "texture2", "texture3",
-           "SoilVol"))
-}
-.getPlotVar<-function(obj, variable, ...) {
-  variable = match.arg(variable, .getAllowedVars())
-  if(variable=="lct") {
-    varplot = factor(obj@lct)
-  } else if(variable=="basalArea") {
-    n = length(obj@forestlist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      f = obj@forestlist[[i]]
-      if(!is.null(f)) varplot[i] = sum(plant_basalArea(f), na.rm=T)
-    }
-  } else if(variable=="SWE") {
-    n = length(obj@soillist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      s = obj@soillist[[i]]
-      if(!is.null(s)) varplot[i] = s[["SWE"]]
-    }
-    varplot = factor(varplot)
-  } else if(variable=="WTD") {
-    n = length(obj@soillist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      s = obj@soillist[[i]]
-      if(!is.null(s)) varplot[i] = soil_waterTableDepth(s, ...)
-    }
-    varplot = factor(varplot)
-  } else if(variable=="texture1") {
-    n = length(obj@soillist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      s = obj@soillist[[i]]
-      if(!is.null(s)) varplot[i] = soil_USDAType(s$clay[1],s$sand[1])
-    }
-    varplot = factor(varplot)
-  } else if(variable=="texture2") {
-    n = length(obj@soillist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      s = obj@soillist[[i]]
-      if(!is.null(s)) varplot[i] = soil_USDAType(s$clay[2],s$sand[2])
-    }
-    varplot = factor(varplot)
-  } else if(variable=="texture3") {
-    n = length(obj@soillist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      s = obj@soillist[[i]]
-      if(!is.null(s)) varplot[i] = soil_USDAType(s$clay[3],s$sand[3])
-    }
-    varplot = factor(varplot)
-  } else if(variable=="SoilVol") {
-    n = length(obj@soillist)
-    varplot = rep(NA, n)
-    for(i in 1:n) {
-      s = obj@soillist[[i]]
-      if(!is.null(s)) varplot[i] = sum(soil_water(s, ...), na.rm=T)
-    }
-  } 
-  return(varplot)
-}
 setMethod("spplot", signature("SpatialPixelsLandscape"),
           function(obj, variable = "lct", ...) {
             if(variable %in% .getAllowedVars()) {
-              spplot(SpatialPixelsDataFrame(as(obj,"SpatialPoints"),
-                                            data.frame(var = .getPlotVar(obj, variable, ...)), 
-                                            grid = obj@grid),
-                     ...)
-              
+              spplot(getLandscapeVariable(obj, variable, ...))
             } else {
               spplot(as(obj, "SpatialPixelsTopography"), variable, ...)
             }
@@ -79,11 +10,7 @@ setMethod("spplot", signature("SpatialPixelsLandscape"),
 setMethod("spplot", signature("SpatialGridLandscape"),
           function(obj, variable = "lct", ...) {
             if(var %in% .getAllowedVars()) {
-              spplot(SpatialGridDataFrame(obj@grid,
-                                          data.frame(var = ,getPlotVar(obj, variable, ...)), 
-                                          proj4string = obj@proj4string),
-                     ...)
-              
+              spplot(getLandscapeVariable(obj, variable, ...))
             } else {
               spplot(as(obj, "SpatialGridTopography"), variable, ...)
             }
@@ -93,11 +20,7 @@ setMethod("spplot", signature("SpatialGridLandscape"),
 setMethod("spplot", signature("SpatialPointsLandscape"),
           function(obj, variable = "lct", ...) {
             if(var %in% .getAllowedVars()) {
-              spplot(SpatialPointsDataFrame(obj@coords,
-                                            data.frame(var = ,getPlotVar(obj, variable, ...)), 
-                                            proj4string = obj@proj4string),
-                     ...)
-              
+              spplot(getLandscapeVariable(obj, variable, ...))
             } else {
               spplot(as(obj, "SpatialPointsTopography"), variable, ...)
             }
@@ -105,20 +28,5 @@ setMethod("spplot", signature("SpatialPointsLandscape"),
 
 setMethod("spplot", signature("DistributedWatershed"),
           function(obj, variable = "lct", ...) {
-            if(variable %in% c("numNeigh", "waterOrder")) {
-              if(variable=="numNeigh") {
-                varplot = sapply(obj@queenNeigh,"length")
-              } else if(variable=="waterOrder") {
-                wo = dw@waterOrder
-                varplot = 1:length(wo)
-                varplot[wo] = 1:length(wo)
-              }
-              spplot(SpatialPixelsDataFrame(as(obj,"SpatialPoints"),
-                                            data.frame(var = varplot), 
-                                            grid = obj@grid),
-                     ...)
-              
-            } else {
-              spplot(as(obj, "SpatialPixelsLandscape"), variable, ...)
-            }
+            spplot(getLandscapeVariable(obj, variable, ...))
           })
