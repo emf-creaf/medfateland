6282b6aca1e3e7b183162e5bb1ca7f06362faeb2
Miquel De Cáceres <miquelcaceres@gmail.com>
2020-09-18 12:47:58 +0200

    getLandscapeVariable

---
+++ R/getLandscapeVariable.R
@@ -0,0 +1,117 @@
+.getAllowedVars <-function() {
+  return(c("lct", "basalArea", "SWE", "WTD","texture1", "texture2", "texture3",
+           "SoilVol"))
+}
+.getLandscapeVar<-function(obj, variable, ...) {
+  variable = match.arg(variable, .getAllowedVars())
+  if(variable=="lct") {
+    varplot = factor(obj@lct)
+  } else if(variable=="basalArea") {
+    n = length(obj@forestlist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      f = obj@forestlist[[i]]
+      if(!is.null(f)) varplot[i] = sum(plant_basalArea(f), na.rm=T)
+    }
+  } else if(variable=="SWE") {
+    n = length(obj@soillist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      s = obj@soillist[[i]]
+      if(!is.null(s)) varplot[i] = s[["SWE"]]
+    }
+    varplot = factor(varplot)
+  } else if(variable=="WTD") {
+    n = length(obj@soillist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      s = obj@soillist[[i]]
+      if(!is.null(s)) varplot[i] = soil_waterTableDepth(s, ...)
+    }
+    varplot = factor(varplot)
+  } else if(variable=="texture1") {
+    n = length(obj@soillist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      s = obj@soillist[[i]]
+      if(!is.null(s)) varplot[i] = soil_USDAType(s$clay[1],s$sand[1])
+    }
+    varplot = factor(varplot)
+  } else if(variable=="texture2") {
+    n = length(obj@soillist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      s = obj@soillist[[i]]
+      if(!is.null(s)) varplot[i] = soil_USDAType(s$clay[2],s$sand[2])
+    }
+    varplot = factor(varplot)
+  } else if(variable=="texture3") {
+    n = length(obj@soillist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      s = obj@soillist[[i]]
+      if(!is.null(s)) varplot[i] = soil_USDAType(s$clay[3],s$sand[3])
+    }
+    varplot = factor(varplot)
+  } else if(variable=="SoilVol") {
+    n = length(obj@soillist)
+    varplot = rep(NA, n)
+    for(i in 1:n) {
+      s = obj@soillist[[i]]
+      if(!is.null(s)) varplot[i] = sum(soil_water(s, ...), na.rm=T)
+    }
+  } 
+  return(varplot)
+}
+setGeneric("getLandscapeVariable", valueClass ="Spatial", function(obj, variable = "lct", ...){
+  standardGeneric("getLandscapeVariable")
+})
+setMethod("getLandscapeVariable", signature("SpatialPixelsLandscape"),
+          function(obj, variable = "lct", ...) {
+            if(variable %in% .getAllowedVars()) {
+              return(SpatialPixelsDataFrame(as(obj,"SpatialPoints"),
+                                            data.frame(var = .getLandscapeVar(obj, variable, ...)), 
+                                            grid = obj@grid))
+              
+            } 
+          })
+
+setMethod("getLandscapeVariable", signature("SpatialGridLandscape"),
+          function(obj, variable = "lct", ...) {
+            if(var %in% .getAllowedVars()) {
+              return(SpatialGridDataFrame(obj@grid,
+                                          data.frame(var = .getLandscapeVar(obj, variable, ...)), 
+                                          proj4string = obj@proj4string))
+              
+            } 
+          })
+
+
+setMethod("getLandscapeVariable", signature("SpatialPointsLandscape"),
+          function(obj, variable = "lct", ...) {
+            if(var %in% .getAllowedVars()) {
+              return(SpatialPointsDataFrame(obj@coords,
+                                            data.frame(var = .getLandscapeVar(obj, variable, ...)), 
+                                            proj4string = obj@proj4string))
+              
+            } 
+          })
+
+setMethod("getLandscapeVariable", signature("DistributedWatershed"),
+          function(obj, variable = "lct", ...) {
+            if(variable %in% c("numNeigh", "waterOrder")) {
+              if(variable=="numNeigh") {
+                varplot = sapply(obj@queenNeigh,"length")
+              } else if(variable=="waterOrder") {
+                wo = dw@waterOrder
+                varplot = 1:length(wo)
+                varplot[wo] = 1:length(wo)
+              }
+              return(SpatialPixelsDataFrame(as(obj,"SpatialPoints"),
+                                            data.frame(var = varplot), 
+                                            grid = obj@grid))
+              
+            } else {
+              return(getVariable(as(obj, "SpatialPixelsLandscape"), variable, ...))
+            } 
+          })
