138cdb5450cdbb8499aec0d29019d013515f0212
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-05-09 18:48:58 +0200

    going real

---
+++ R/initialize_landscape.R
@@ -0,0 +1,137 @@
+#' Initialization of model inputs for spatially-distributed forest stands
+#' 
+#' Initializes state for local models \code{\link{spwb}} or \code{\link{growth}}. 
+#' 
+#' @param x An object of class \code{\link{sf}} with the following columns:
+#'   \itemize{
+#'     \item{\code{geometry}: Spatial geometry.}
+#'     \item{\code{forest}: Objects of class \code{\link{forest}}.}
+#'     \item{\code{soil}: Objects of class \code{\link{soil}} or data frames of physical properties.}
+#'     \item{\code{land_cover_type}: Land cover type of each grid cell (values should be 'wildland' or 'agriculture').}
+#'     \item{\code{crop_factor}: Crop evapo-transpiration factor. Only required for 'agriculture' land cover type.}
+#'     \item{\code{local_control}: A list of control parameters (optional). Used to override function parameter \code{local_control} for specific cells (values can be \code{NULL} for the remaining ones).}
+#'   }
+#' @param SpParams A data frame with species parameters (see \code{\link{SpParamsMED}}).
+#' @param local_control A list of control parameters (see \code{\link{defaultControl}}).
+#' @param model A string to indicate the model, either \code{"spwb"} or \code{"growth"}.
+#' @param replace Boolean flag to replace existing initialized states
+#' @param progress Boolean flag to display progress information.
+#' 
+#' @returns Replaces or adds column 'state' whose elements are \code{\link{spwbInput}} or \code{\link{growthInput}} objects 
+#' and returns the modified object of class 'sf'.
+#' 
+#' @details
+#' Initialization is dealt automatically when calling simulation functions \code{\link{spwb_spatial}},  \code{\link{growth_spatial}},
+#' \code{\link{spwb_spatial_day}} or \code{\link{growth_spatial_day}}. However, function \code{initialize_landscape}  allows separating initialization from model simulations.
+#' 
+#' @author Miquel De \enc{Cáceres}{Caceres} Ainsa, CREAF
+#' 
+#' @seealso 
+#' \code{\link{spwb_spatial}}, \code{\link{spwb_spatial_day}}, 
+#' \code{\link{update_landscape}}
+#' 
+#' @examples
+#' # Load example landscape data
+#' data("example_ifn")
+#'   
+#' # Load example meteo data frame from package meteoland
+#' data("examplemeteo")
+#'   
+#' # Load default medfate parameters
+#' data("SpParamsMED")
+#'   
+#' # Initialize state for 'spwb' simulations
+#' example_ifn_init <- initialize_landscape(example_ifn, SpParamsMED, 
+#'                                          defaultControl(), model = "spwb")
+#' 
+#' @name initialize_landscape
+#' @export
+initialize_landscape<- function(x, SpParams, local_control, model = "spwb", replace = FALSE, progress = TRUE) {
+  match.arg(model, c("spwb", "growth"))
+  if(!inherits(x, "sf")) cli::cli_abort("'x' has to be an object of class 'sf'.")
+  if(!("forest" %in% names(x))) cli::cli_abort("Column 'forest' must be defined.")
+  if(!("soil" %in% names(x))) cli::cli_abort("Column 'soil' must be defined.")
+  forestlist = x$forest
+  soillist  = x$soil
+  n <- length(forestlist)
+  if("state" %in% names(x)) {
+    xlist  = x$state
+  } else {
+    xlist = vector("list",n)
+  }
+  if("land_cover_type" %in% names(x)) {
+    landcover <- x$land_cover_type
+  } else {
+    landcover <- rep("wildland", n)
+  }
+  if("crop_factor" %in% names(x)) {
+    cropfactor <- x$crop_factor
+  } else {
+    cropfactor <- rep(NA, n)
+  }
+  n = length(forestlist)
+  if(model %in% c("spwb", "growth")) {
+    init<-rep(FALSE, n)
+    for(i in 1:n) {
+      if(landcover[i] == "wildland") {
+        f = forestlist[[i]]
+        s = soillist[[i]]
+        if(inherits(f, "forest") && inherits(s, c("soil", "data.frame"))) {
+          init[i] = TRUE
+          x_i = xlist[[i]]
+          if(!replace) {
+            if(inherits(x_i,"spwbInput") && model=="spwb") init[i] = FALSE
+            if(inherits(x_i,"growthInput") && model=="growth") init[i] = FALSE
+          }
+        }
+      } else if(landcover[i] == "agriculture") {
+        if(inherits(soillist[[i]], c("soil","data.frame"))) {
+          init[i] <- TRUE
+        }
+      }
+    }
+    w_init = which(init)
+    if(length(w_init)>0) {
+      if(progress) { 
+        cli::cli_progress_step(paste0("Creating ", length(w_init), " input objects for model '", model, "'."))
+        cli::cli_progress_bar(name = "Stands", total = n)
+      }
+      for(w in 1:length(w_init)) {
+        i = w_init[w]
+        s = soillist[[i]]
+        local_control_i <- NULL
+        if("local_control" %in% names(x)) {
+          if(!is.null(x$local_control[[i]])) {
+            if(inherits(x$local_control[[i]], "list")) local_control_i <- x$local_control[[i]]
+          }
+        }
+        if(is.null(local_control_i)) local_control_i <- local_control
+        if(inherits(s, "data.frame")) {
+          if(any(is.na(s$clay))) cli::cli_abort(paste0("Missing 'clay' values in soil #", i, ". Please correct."))
+          if(any(is.na(s$sand))) cli::cli_abort(paste0("Missing 'sand' values in soil #", i, ". Please correct."))
+          if(any(is.na(s$bd))) cli::cli_abort(paste0("Missing 'bd' values in soil #", i, ". Please correct."))
+          if(any(is.na(s$rfc))) cli::cli_abort(paste0("Missing 'rfc' values in soil #", i, ". Please correct."))
+          s <- soil(s)
+        }
+        if(landcover[i] == "wildland") {
+          f = forestlist[[i]]
+          if(inherits(f, "forest") && inherits(s, "soil")) {
+            if(model=="spwb") {
+              xlist[[i]] = medfate::forest2spwbInput(f, s, SpParams, local_control_i)
+            } else if(model=="growth") {
+              xlist[[i]] = medfate::forest2growthInput(f, s, SpParams, local_control_i)
+            }
+          }
+        } else if(landcover[i] == "agriculture") {
+          xlist[[i]] <- medfate::aspwbInput(crop_factor = cropfactor[i], control = local_control_i, soil = s)
+        }
+        if(progress) cli::cli_progress_update()
+      }
+      if(progress) cli::cli_progress_done()
+    } else {
+      if(progress) cli::cli_alert_info(paste0("All input objects are already available for '", model, "'. Use 'replace = TRUE' to force replacing current inputs."))
+    }
+  } 
+  x[["state"]] <- xlist
+  return(x)
+}
