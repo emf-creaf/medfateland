07802f2d1a690df31bfa297c0f618f6501148674
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-05-11 12:06:59 +0200

    verbose to progress

--- R/create_landscape.R
+++ R/create_landscape.R
@@ -6,7 +6,7 @@
 #' @param dem A digital elevation model (class \code{\link{SpatRaster}}) with meters as units
 #' @param land_cover_map An object of class \code{\link{SpatRaster}} of land cover type. If missing, all locations are considered 'wildland'.
 #' @param wildland,agriculture,rock,artificial,water Strings indicating the mapping from the legend of land_cover_map. 
-#' @param verbose A logical flag to print console output
+#' @param progress A logical flag to print console output
 #'
 #' @details
 #' The user should manually define the mapping of land cover classes in \code{land_cover_map} to the land cover types 
@@ -29,17 +29,17 @@
 #' }
 create_landscape<-function(x, dem, land_cover_map = NULL, 
                            wildland = NULL, agriculture = NULL, rock = NULL, artificial = NULL, water = NULL,
-                           verbose = TRUE) {
-  if(verbose) cli::cli_progress_step("Checking inputs")
+                           progress = TRUE) {
+  if(progress) cli::cli_progress_step("Checking inputs")
   if(!inherits(x, "sf")) cli::cli_abort("'x' should be of class 'sf' ")
   if(!inherits(dem, "SpatRaster")) cli::cli_abort("'dem' should be of class 'SpatRaster'")
   if(!is.null(land_cover_map)) if(!inherits(dem, "SpatRaster")) cli::cli_abort("'dem' should be of class 'SpatRaster'")
   
   if(!("id" %in% names(x))) {
-    if(verbose) cli::cli_progress_step("Defining column 'id'")
+    if(progress) cli::cli_progress_step("Defining column 'id'")
     x$id <- 1:nrow(x)
   }
-  if(verbose) cli::cli_progress_step("Extracting topography")
+  if(progress) cli::cli_progress_step("Extracting topography")
   x_vect <- terra::vect(sf::st_transform(x, terra::crs(dem)))
   x$elevation <- terra::extract(dem, x_vect)[,2]
   slope <- terra::terrain(dem, v = "slope", unit = "degrees")
@@ -47,7 +47,7 @@ create_landscape<-function(x, dem, land_cover_map = NULL,
   aspect <- terra::terrain(dem, v = "aspect", unit = "degrees")
   x$aspect <- terra::extract(aspect, x_vect)[,2]
   if(!is.null(land_cover_map)) {
-    if(verbose) cli::cli_progress_step("Extracting land cover")
+    if(progress) cli::cli_progress_step("Extracting land cover")
     x_lcm <- terra::vect(sf::st_transform(x, terra::crs(land_cover_map)))
     x$land_cover_type <- NA
     lct <- terra::extract(land_cover_map, x_lcm)[,2]
@@ -59,6 +59,6 @@ create_landscape<-function(x, dem, land_cover_map = NULL,
   } else {
     x$land_cover_type <- "wildland"
   }
-  if(verbose) cli::cli_progress_done()
+  if(progress) cli::cli_progress_done()
   return(sf::st_as_sf(tibble::as_tibble(x)))
 }
