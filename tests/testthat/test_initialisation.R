67b6b596495395ff5128b436145b12991f7d158a
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-09-18 13:21:10 +0200

    clean check2

--- tests/testthat/test_initialisation.R
+++ tests/testthat/test_initialisation.R
@@ -1,12 +1,12 @@
 library(medfateland)
-
-test_that("Can build from forestables",{
-  testthat::skip_on_ci()
-  testthat::skip_on_cran()
-  library(forestables)
-  ifn4_example <- ifn_output_example |>
-    dplyr::filter(version == "ifn4")
-  expect_s3_class(parse_forestable(ifn4_example[1:4,]), "sf")
-  expect_s3_class(parse_forestable(ifn4_example[1:4,], keepUnfilteredCopy = TRUE), "sf")
-})
+# 
+# test_that("Can build from forestables",{
+#   testthat::skip_on_ci()
+#   testthat::skip_on_cran()
+#   library(forestables)
+#   ifn4_example <- ifn_output_example |>
+#     dplyr::filter(version == "ifn4")
+#   expect_s3_class(parse_forestable(ifn4_example[1:4,]), "sf")
+#   expect_s3_class(parse_forestable(ifn4_example[1:4,], keepUnfilteredCopy = TRUE), "sf")
+# })
 
